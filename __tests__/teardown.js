00967ce0cdb192f76098d83df49b65fe5712674a
Jhen Nuñez <jxelektro@gmail.com>
2022-10-24 23:12:10 -0300

    Executing (default): DROP DATABASE IF EXISTS postgres

--- __tests__/teardown.js
+++ __tests__/teardown.js
@@ -1,7 +1,11 @@
 import db from '../config/database.js';
 
-// This is the teardown function that will be called after each test
+//close the database connection before the test begins
+before(async () => {
+  await db.sequelize.sync({ force: true });
+});
 
+// This is the teardown function that will be called after each test
 after(async () => {
   await db.close();
 });
--- config/config.js
+++ config/config.js
@@ -1,6 +1,11 @@
 
-import * as dotenv from 'dotenv';
-dotenv.config();
+function runDotEnv() {
+  if (process.env.ENV === 'development') {
+    import('dotenv').then(dotenv => dotenv.config({ path: '.env' }));
+  }
+}
+
+runDotEnv();
 
 
 const config = {
@@ -9,17 +14,16 @@ const config = {
     password: process.env.DB_DEV_PASSWORD,
     database: process.env.DB_DEV_DB_NAME,
     host: process.env.DB_DEV_HOST,
-    logging: console.log,
     dialect: 'postgres',
   },
   test: {
-    username: /*process.env.DB_TEST_USER || */'postgres',
-    password: /*process.env.DB_TEST_PASSWORD || */'alcap1990',
-    database: /*process.env.DB_TEST_DB_NAME || */'postgres',
-    host: /* process.env.DB_TEST_HOST || */'localhost',
-    port: /* process.env.DB_TEST_PORT || */ 5432,
-    logging: false,
-    dialect: 'postgres',
+    username: process.env.DB_TEST_USER || 'postgres',
+    password: process.env.DB_TEST_PASSWORD || 'alcap1990',
+    database: process.env.DB_TEST_DB_NAME || 'postgres',
+    host:  process.env.DB_TEST_HOST || 'localhost',
+    port: process.env.DB_TEST_PORT ||  5432,
+    logging: true,
+    dialect: 'postgres'
   },
   production: {
     username: process.env.DB_DEV_USER,
