00878cb5c2de3635ad0f229089bc683d80b933fd
Jhen Nuñez <jxelektro@gmail.com>
2022-10-24 21:21:27 -0300

    ERROR: no se puede eliminar la base de datos activa

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 
 const config = {
-  development: {
+ development: {
     username: process.env.DB_DEV_USER,
     password: process.env.DB_DEV_PASSWORD,
     database: process.env.DB_DEV_DB_NAME,
@@ -13,11 +13,11 @@ const config = {
     dialect: 'postgres',
   },
   test: {
-    username: process.env.DB_TEST_USER || 'postgres',
-    password: process.env.DB_TEST_PASSWORD || 'alcap1990',
-    database: process.env.DB_TEST_DB_NAME || 'postgres',
-    host: process.env.DB_TEST_HOST || 'localhost',
-    port: process.env.DB_TEST_PORT || 5432,
+    username: /*process.env.DB_TEST_USER || */'postgres',
+    password: /*process.env.DB_TEST_PASSWORD || */'alcap1990',
+    database: /*process.env.DB_TEST_DB_NAME || */'postgres',
+    host: /* process.env.DB_TEST_HOST || */'localhost',
+    port: /* process.env.DB_TEST_PORT || */ 5432,
     logging: false,
     dialect: 'postgres',
   },
