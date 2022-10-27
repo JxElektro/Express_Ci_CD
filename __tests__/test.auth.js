229fad035ce01f84f7bd8daf45771602be0fcdd9
Jhen Nuñez <jxelektro@gmail.com>
2022-10-27 19:42:28 -0300

    Adding dealership Test

--- __tests__/test.auth.js
+++ __tests__/test.auth.js
@@ -2,6 +2,8 @@ import request from 'supertest';
 import chai from 'chai';
 import app from '../app1.js';
 import User from '../models/user.js';
+import getToken from './utils.js';
+
 
 const { expect } = chai;
 
@@ -9,6 +11,7 @@ const { expect } = chai;
 
 describe('Test endpoint auth/register', () => {
   it('The enpoint has a 201 status and the obj cant be null', async () => {
+    let token = await getToken();
     const payload = {
       'name': 'teast',
       'email': 'test@gmail.com',
@@ -18,7 +21,7 @@ describe('Test endpoint auth/register', () => {
     // pretends to send a post to the endpoint auth/register with the payload and await the response
     const { body, status } = await request(app).post('/auth/register').send(payload);
     expect(status).to.equal(201);
-    
+
     // check if the user was created in the database
     expect(body).to.have.property('userId');
     const userId = body.userId;
@@ -34,48 +37,48 @@ describe('Test endpoint auth/register', () => {
     // pretends to send a post to the endpoint auth/register with the payload and await the response
     const { body, status } = await request(app).post('/auth/register').send(payload);
     expect(status).to.equal(400);
-});
-it('should allow to login', async () => {
-  const payload = {
-    'email': 'josae@email.com',
-    'password': '123456'
-  }
-  const { body, status } = await request(app)
-    .post('/auth/login')
-    .type("json")
-    .send(payload);
-  expect(status).to.equal(200);
-  //expect a message "message": "Login successfull"
-  expect(body).to.have.property('message');
-  expect(body.message).to.equal('Login successfull');
-});
+  });
+  it('should allow to login', async () => {
+    const payload = {
+      'email': 'josae@email.com',
+      'password': '123456'
+    }
+    const { body, status } = await request(app)
+      .post('/auth/login')
+      .type("json")
+      .send(payload);
+    expect(status).to.equal(200);
+    //expect a message "message": "Login successfull"
+    expect(body).to.have.property('message');
+    expect(body.message).to.equal('Login successfull');
+  });
 
-it("should fail if email is not incorrect", async () => {
-  const payload = {
-    'email': 'unexsitedEmail@email.com',
-    'password': '123456'
-  }
-  
-  const { body, status } = await request(app)
-    .post('/auth/login')
-    .type("json")
-    .send(payload);
-  expect(status).to.equal(404);
-  // expect body to have a message
-  expect(body.message).contains('No user found with email');
-});
+  it("should fail if email is not incorrect", async () => {
+    const payload = {
+      'email': 'unexsitedEmail@email.com',
+      'password': '123456'
+    }
 
-it("should fail if password is not incorrect", async () => {
-  const payload = {
-    'email': 'josae@email.com',
-    'password': 'badPassword'
-  }
-  const { body, status } = await request(app)
-    .post('/auth/login')
-    .type("json")
-    .send(payload);
-  expect(status).to.equal(401);
-  // expect body to have a message
-  expect(body.message).contains('Invalid Password');
-});
+    const { body, status } = await request(app)
+      .post('/auth/login')
+      .type("json")
+      .send(payload);
+    expect(status).to.equal(404);
+    // expect body to have a message
+    expect(body.message).contains('No user found with email');
+  });
+
+  it("should fail if password is not incorrect", async () => {
+    const payload = {
+      'email': 'josae@email.com',
+      'password': 'badPassword'
+    }
+    const { body, status } = await request(app)
+      .post('/auth/login')
+      .type("json")
+      .send(payload);
+    expect(status).to.equal(401);
+    // expect body to have a message
+    expect(body.message).contains('Invalid Password');
+  });
 });
--- __tests__/test.dealership.js
+++ __tests__/test.dealership.js
@@ -1,24 +1,43 @@
- /*import request from 'supertest';
-  import chai from 'chai';
-  import app from '../app.js';
-  import Dealership from '../models/dealership.js';
-  import getToken from './utils.js';
-  import { before } from 'mocha';
-
-  const { expect } = chai;
-
+import request from 'supertest';
+import chai from 'chai';
+import app from '../app1.js';
+import User from '../models/user.js';
+import getToken from './utils.js';
+import Dealership from '../models/dealership.js';
+import { before } from 'mocha';
 
 
-  describe('Test the auth endpoints', () => {
+const { expect } = chai;
 
-    it('should allow to create users', async () => {
-      const payload = {
-        'name': 'teast',
-        'last_name': 'test',
-        'email': 'josae@email.com',
-        'password': '123456'
-      }
-      const { body, status } = await request(app).post('/auth/register').send(payload);
-      expect(status).to.equal(201);
+// test the enpoint of dealerships
+
+describe('Test endpoint dealerships', () => {
+  let token;
+  let dealership1;
+  let dealership2;
+
+  before(async () => {
+    token = await getToken();
+    dealership1 = await Dealership.create({
+      name: 'Dealership 1',
+      address: 'Address 1',
+      city: 'City 1',
     });
-  });*/
+    dealership2 = await Dealership.create({
+      name: 'Dealership 2',
+      address: 'Address 2',
+      city: 'City 2',
+    });
+
+    it('should get all dealerships', async () => {
+      const { body, status } = await request(app)
+        .get('/dealerships')
+        .set('Authorization', `Bearer ${token}`);
+      expect(status).to.equal(200);
+      expect(body).to.be.an('array');
+      expect(body.length).to.equal(2);
+      expect(body[0].name).to.equal(dealership1.name);
+      expect(body[1].name).to.equal(dealership2.name);
+    });
+  });
+});
