d118b1c238744fd00d504311e3f0432d7e5cb013
Jhen Nuñez <jxelektro@gmail.com>
2022-10-27 20:39:36 -0300

    9Try Whitout .env

--- config/database.js
+++ config/database.js
@@ -3,12 +3,12 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME || 'postgres',
+  process.env.DB_USER || 'postgres',
+  process.env.DB_PASSWORD || 'alcap1990',
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: process.env.DB_HOST|| 'localhost',
+    port: process.env.DB_PORT|| 5432,
     dialect: "postgres",
     logging: false
   }
