5a9beb20f545cdd8d9f9f92abc9b613b24534239
Jhen Nuñez <jxelektro@gmail.com>
2022-10-27 20:06:42 -0300

    ci 1try

--- __tests__/test.dealership.js
+++ __tests__/test.dealership.js
@@ -1,7 +1,6 @@
 import request from 'supertest';
 import chai from 'chai';
 import app from '../app1.js';
-import User from '../models/user.js';
 import getToken from './utils.js';
 import Dealership from '../models/dealership.js';
 import { before } from 'mocha';
